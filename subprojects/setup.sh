9e3cfab441075d07e9537fb5339867d87ee2a339
ksqsf <i@ksqsf.moe>
2026-03-24 16:48:57 +0100

    ci: build with different lua versions

--- subprojects/setup.sh
+++ subprojects/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 cd subprojects/librime
+
+# patch opencc
+echo "#include<cstdint>" | cat - deps/opencc/src/SerializedValues.hpp > temp
+mv temp deps/opencc/src/SerializedValues.hpp
+
 make deps
 ./install-plugins.sh hchunhui/librime-lua
 ./install-plugins.sh rime/librime-predict
